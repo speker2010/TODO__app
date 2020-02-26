3c368ed88c546926f2a108f1a9539130a7cda6e8
Artem Besedin <speker@mail.ru>
2020-02-26 22:21:29 +0400

    Update gulpfile.js
    Add styles task to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,24 @@ var autoprefixer = require('gulp-autoprefixer');
 var sourcemaps = require('gulp-sourcemaps');
 var browserSync = require('browser-sync').create();
 
+function stylesScss(done) {
+  gulp.src('./scss/**/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+      errorLogToConsole: true,
+      outputStyle: 'compressed'
+    }))
+    .on('error', console.error.bind(console))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(sourcemaps.write('./'))
+    .pipe( gulp.dest('./css/') )
+  done();
+}
+
 function styles(done) {
   gulp.src('./scss/**/*.scss')
     .pipe(sourcemaps.init())
@@ -56,6 +74,7 @@ function watchFiles() {
 }
 
 gulp.task('default', gulp.parallel(sync, watchFiles));
+gulp.task('styles', stylesScss);
 gulp.task(sync);
 
 
